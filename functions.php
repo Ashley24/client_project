0919d2087aa0a8725f312538b99b48e90a2740df
Ashley Gibson <ashleyg2431@tywls.org>
2016-07-29 10:04:26 -0400

    css will be changed

--- functions.php
+++ functions.php
@@ -11,3 +11,33 @@ function my_theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
+
+
+<!-- <input id="box-toggle" type="checkbox">
+<label for="box-toggle">Show geometry</label>  -->
+
+<!-- 
+<</ul> -->
+<!-- <ul class="nav nav-pills nav-stacked">
+   <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Background</a></li>
+  <li role="presentation"><a href="#">Services</a></li>
+</ul> -->
+
+
+<body data-spy="scroll" data-target="#navbar-example">
+ 
+  <div id="navbar-example">
+    <ul class="nav nav-tabs" role="tablist">
+     <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Background</a></li>
+  <li role="presentation"><a href="#">Services</a></li>
+    </ul>
+  </div>
+  ...
+</body>
+
+
+
+
+
